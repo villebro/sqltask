52659de95e5887e6e6dab53ef592c8a1cc9445e9
Ville Brofeldt <ville.brofeldt@iki.fi>
2019-09-17 14:27:43 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![PyPI version](https://img.shields.io/pypi/v/sqltask.svg)](https://badge.fury.io/py/sqltask)
+[![PyPI](https://img.shields.io/pypi/pyversions/sqltask.svg)](https://www.python.org/downloads/)
+[![PyPI license](https://img.shields.io/pypi/l/sqltask.svg)](https://opensource.org/licenses/MIT)
 # sqltask
 ETL tool for performing mostly SQL-based data transformation
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sqltask',
+    version='0.0.2',
+    description='ETL tool for performing mostly SQL-based data transformation',
+    long_description='',
+    url='https://github.com/villebro/sqltask',
+    author='Ville Brofeldt',
+    author_email='villebro@apache.org',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
