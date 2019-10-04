940cbd95783c2906eca1737198a4d421d28623c0
Ville Brofeldt <ville.brofeldt@iki.fi>
2019-10-04 15:11:49 +0300

    Encode csv as utf-8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='sqltask',
-    version='0.0.4',
+    version='0.0.5',
     description='ETL tool for performing mostly SQL-based data transformation',
     long_description='',
     url='https://github.com/villebro/sqltask',
