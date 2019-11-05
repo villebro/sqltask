4dd3c85b81ec879b9e2d6fb24885305b94ea3c56
Ville Brofeldt <ville.brofeldt@iki.fi>
2019-11-05 12:48:20 +0200

    Fix eager lookup bug

--- sqltask/__init__.py
+++ sqltask/__init__.py
@@ -5,7 +5,7 @@ from sqltask.base.lookup_source import BaseLookupSource
 from sqltask.base.row_source import BaseRowSource
 from sqltask.base.table import BaseOutputRow, BaseTableContext
 
-__version__ = '0.4.3'
+__version__ = '0.4.4'
 
 # initialize logging
 logger = logging.getLogger(__name__)
